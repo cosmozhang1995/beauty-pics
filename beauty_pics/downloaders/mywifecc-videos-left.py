9040ca22c05b681199e9a87d02da47322cd59c2e
Cosmo Zhang <cosmozhang1995@gmail.com>
2018-06-13 13:18:47 +0800

    dev

--- beauty_pics/downloaders/mywifecc-videos-left.py
+++ beauty_pics/downloaders/mywifecc-videos-left.py
@@ -4,9 +4,6 @@ import os
 import sys
 sys.path.append(os.path.realpath(os.path.join(os.path.dirname(__file__), "../..")))
 from config import user_config, UserConfig
-import requests
-import progressbar
-import traceback
 
 rooturl = user_config.get("mywifecc", "root_url")
 saving_dir = user_config.get("mywifecc", "saving_dir")
@@ -16,8 +13,8 @@ info_filename = user_config.get("mywifecc", "info_filename")
 
 dirs = os.listdir(saving_dir)
 
-sess = requests.session()
-sess.post(rooturl + "/login/comp", { "login_id": username, "passwd": password })
+# sess = requests.session()
+# sess.post(rooturl + "/login/comp", { "login_id": username, "passwd": password })
 
 prgbar = None
 tmpfile = None
@@ -27,6 +24,7 @@ downloaded = 0
 
 for dirname in dirs:
   info = UserConfig(os.path.join(saving_dir, dirname, info_filename))
+  filenames = os.listdir(os.path.join(saving_dir, dirname))
   number = info.get("number")
   pageurl = info.get("pageurl")
   def parse_videourl(url):
@@ -37,15 +35,19 @@ for dirname in dirs:
     return url
   videourl1 = parse_videourl(info.get("video"))
   videourl2 = parse_videourl(info.get("video2"))
-  for videourl in [videourl1, videourl2]:
-    if videourl is None:
-      continue
-    r = sess.head(videourl, allow_redirects=True)
-    filename = re.match(r"^.*\/([^\/]+)$", r.url).group(1).lower()
-    filepath = os.path.join(saving_dir, dirname, filename)
-    total += 1
-    if os.path.exists(filepath):
-      downloaded += 1
+  total_of_this = len(filter(lambda x: not x is None, [videourl1, videourl2]))
+  downloaded_of_this = len(filter(lambda x: not re.match(r".*\.(mp4)", x) is None, filenames))
+  # for videourl in [videourl1, videourl2]:
+  #   if videourl is None:
+  #     continue
+  #   r = sess.head(videourl, allow_redirects=True)
+  #   filename = re.match(r"^.*\/([^\/]+)$", r.url).group(1).lower()
+  #   filepath = os.path.join(saving_dir, dirname, filename)
+  #   total += 1
+  #   if os.path.exists(filepath):
+  #     downloaded += 1
+  total += total_of_this
+  downloaded += downloaded_of_this
 
 
 print "%d of %d videos downloaded" % (downloaded, total)
